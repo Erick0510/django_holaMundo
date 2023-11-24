1bf5bf14ebf33b476d9918b60bcbe44da8aa22fc
Erick <erick_3041210064@utd.edu.mx>
2023-11-24 15:55:07 -0600

    v2

--- hello_world_project/my_app/views.py
+++ hello_world_project/my_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hola mundo, soy Erick")
+    return HttpResponse("Hola mundo, soy Evelyn, y este es mi examen")
 # Create your views here.
